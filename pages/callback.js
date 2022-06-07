c949fa6c009fdca5799dae0c39fddaf7d1ce81d6
Cole <cole@colevscode.io>
2022-06-06 18:34:41 -0700

    simplifying

--- pages/callback.js
+++ pages/callback.js
@@ -1,34 +1,51 @@
-import { completeOAuthProcess } from "../utils/oauth";
+import qs from "query-string";
+
+async function getVercelToken(code, next) {
+  const body = qs.stringify({
+    client_id: process.env.VRCL_CLIENT_ID,
+    client_secret: process.env.VRCL_CLIENT_SECRET,
+    code,
+    redirect_uri: next,
+  });
+
+  const response = await fetch(`https://api.vercel.com/v2/oauth/access_token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+    body,
+  });
+
+  if (response.status !== 200) {
+    throw new Error(
+      `${response.status} ERROR: ${(await response.json())?.error?.message}`
+    );
+  }
+
+  return await response.json();
+}
 
 export const getServerSideProps = async (context) => {
   const { next, code } = context.query;
-  let result;
-
   try {
-    result = await completeOAuthProcess(
-      code,
-      `https://api.vercel.com/v2/oauth/access_token`,
-      process.env.VRCL_CLIENT_ID,
-      process.env.VRCL_CLIENT_SECRET,
-      next
-    );
+    const result = await getVercelToken(code, next);
+    return {
+      props: { result, next },
+    };
   } catch (error) {
-    result = { error: error.message };
+    return { props: { result: { error: error.message } } };
   }
-
-  return {
-    props: { result, next },
-  };
 };
 
-export default function Page({ result, next }) {
+export default ({ result, next }) => {
   return (
     <div style={{ fontFamily: "sans-serif", margin: "auto 2em" }}>
       <h2>Result:</h2>
       <pre style={{ fontFamily: "mono", paddingBottom: "2em" }}>
         {JSON.stringify(result, null, 2)}
       </pre>
-      <a href={`${next}`}>Add Integration</a>
+      {next && <a href={next}>Add Integration</a>}
     </div>
   );
-}
+};
